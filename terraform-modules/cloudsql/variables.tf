178028b888c779a879c113f8e6a5589bfe4ebb84
Peddolla <ShashidharReddy.Peddolla@innovasolutions.com>
2026-02-27 11:55:40 +0530

    gcp infra setup files

--- terraform-modules/cloudsql/variables.tf
+++ terraform-modules/cloudsql/variables.tf
@@ -1,7 +1,7 @@
-# variable "product_name" {}
-# variable "environment" {}
-# variable "region" {}
-# variable "db_version" {}
-# variable "db_tier" {}
-# variable "network_id" {}
-# variable "kms_key" {}
+variable "product_name" {}
+variable "environment" {}
+variable "region" {}
+variable "db_version" {}
+variable "db_tier" {}
+variable "network_id" {}
+variable "kms_key" {}
